42011ba9a9c656dbf7329263db3d574ce295395e
Alec Chen <alec@cpan.org>
2013-02-26 14:34:12 +0800

    more expectations

--- turtle_unittest.cc
+++ turtle_unittest.cc
@@ -3,11 +3,28 @@
 #include "gtest/gtest.h"
 
 using ::testing::AtLeast;                    
+using ::testing::Return;
 
-TEST(TurtleTest, CallPenDown) {
+TEST(TurtleTest, PenDown) {
   MockTurtle turtle;                         
+
   EXPECT_CALL(turtle, PenDown())             
     .Times(AtLeast(1));
-
   turtle.PenDown();
 }                                             
+
+TEST(TurtleTest, GetX) {
+  MockTurtle turtle;                         
+
+  EXPECT_CALL(turtle, GetX())
+    .Times(5)
+    .WillOnce(Return(100))
+    .WillOnce(Return(150))
+    .WillRepeatedly(Return(200));
+
+  EXPECT_EQ(turtle.GetX(), 100);
+  EXPECT_EQ(turtle.GetX(), 150);
+  EXPECT_EQ(turtle.GetX(), 200);
+  EXPECT_EQ(turtle.GetX(), 200);
+  EXPECT_EQ(turtle.GetX(), 200);
+}
